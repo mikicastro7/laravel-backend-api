3c13e759ac8dac9f82ca59102c4319d0beb8e1f3
mikicastro7 <castro-miquel@hotmail.com>
2020-11-19 22:00:41 +0100

    sending error login

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -32,7 +32,7 @@ class AuthController extends Controller
         ]);
 
         if (!auth()->attempt($loginData)) {
-            return response(['message' => 'Invalid Credentials']);
+            return response(['message' => 'Invalid Credentials'], 401);
         }
 
         $accessToken = auth()->user()->createToken('authToken')->accessToken;
